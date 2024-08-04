c461eba3c289bd6aaf0fecf53426f29846abfcab
Emanuel Bitencourt <emanuelmagalhaes3m@gmail.com>
2024-08-04 01:11:38 -0300

    add animation intro

--- src/components/Home/Intro/index.tsx
+++ src/components/Home/Intro/index.tsx
@@ -2,12 +2,20 @@ import introducao from "../../../assets/imgs/fotos/introducao.jpg";
 import "../../../styles/global.scss";
 import "./intro.scss";
 import PainelText from "./PainelText";
+import { useSpring, animated } from "@react-spring/web";
 
 const Intro: React.FC = () => {
+  const fade = useSpring({
+    y: 0,
+    opacity: 1,
+    from: { y: -100, opacity: 0 },
+    delay: 200,
+  });
+
   return (
     <main className="intro-bg ">
       <div className="container ">
-        <div className="intro">
+        <animated.div style={fade} className="intro">
           <PainelText />
           <picture className="">
             <source media="(max-width: 800px)" srcSet={introducao} />
@@ -18,7 +26,7 @@ const Intro: React.FC = () => {
               alt="Bicicleta elétrica preta."
             />
           </picture>
-        </div>
+        </animated.div>
       </div>
     </main>
   );
